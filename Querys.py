b443ee8cf9b1cf3bcaedf61c55c9fbc1fc91bc14
JoseRPrietoF <prietofontcuberta@gmail.com>
2017-09-26 12:04:30 +0200

    Documentation and more querys

--- Querys.py
+++ Querys.py
@@ -1,4 +1,21 @@
-"""limits: http://docs.aws.amazon.com/es_es/amazondynamodb/latest/developerguide/HowItWorks.ProvisionedThroughput.html"""
+"""limits: http://docs.aws.amazon.com/es_es/amazondynamodb/latest/developerguide/HowItWorks.ProvisionedThroughput.html
+
+List methods:
+get metadata from table : get_table_metadata(table_name)
+read item (one atrib) from table by pk: scan_table(table_name, filter_key, filter_value)
+the same but by any key: scan_table(table_name, filter_key, filter_value)
+list all users: users_list()
+get events between two dates: actions_between_time(time1, time2)
+services used by an user: used_services(user, time1, time2)
+list of all services used from an user (return list_events(dict) and count_events(list)): user_count_event(user, event, time1, time2)
+list a top user using services. 
+    -> Return dict (k:user_name, v: list of events) and ordered list ((user_name,number_actions),..): top_users(time1, time2, event=None)
+
+
+aux methods:
+search in a list of events (dict format): search_in_events(dict, events, attrib)
+format/validate time format: format_time(time)
+"""
 
 from boto3 import resource
 from boto3.dynamodb.conditions import Key
@@ -135,8 +152,7 @@ def format_time(time):
 """all actions between time1 and time2
 time = YYYY-MM-DD or YYYY-MM-DDTHH-MM-SSZ,
 Return, userIdentity_userName, eventName, Evntsource, eventTime """
-def actions_between_time(time1, time2):
-
+def actions_between_time(time1, time2, feAux = None):
     time1 = format_time(time1)
     time2 = format_time(time2)
 
@@ -148,19 +164,20 @@ def actions_between_time(time1, time2):
     pe = users_itemName + ", " + eventName + ", " + eventSource + ", " + eventTime
     #filter expression
     fe = Key(eventTime).between(time1, time2);
-
     table = dynamodb_resource.Table(table_name)
+
+    if feAux is not None:
+        fe_complete = fe & feAux
+
     response = table.scan(
         ProjectionExpression = pe,
         # ScanFilter=Key('eventTime').between(time1, time2)
-        FilterExpression=fe,
+        FilterExpression=fe_complete,
     )
     events = response['Items']
     while 'LastEvaluatedKey' in response:
-        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'],ProjectionExpression=pe,FilterExpression=fe,)
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'],ProjectionExpression=pe,FilterExpression=fe_complete,)
         events.extend(response['Items'])
-
-        # print(users)
 
     return events
 
@@ -209,11 +226,83 @@ def used_services(user, time1 = None, time2 = None):
 
     return events
 
-"""Count events between to time from an user"""
+"""Count events from an user
+Return events, number_of_events"""
 def user_count_event(user, event, time1, time2):
     event_search = 'eventName'
+    time1 = format_time(time1)
+    time2 = format_time(time2)
+
+    users_itemName = 'userIdentity_userName'
+    eventName = 'eventName'
+    eventSource = 'eventSource'
+    eventTime = 'eventTime'
+
+    pe = users_itemName + ", " + eventName + ", " + eventSource + ", " + eventTime
+    # filter expression
+    fe2 = Key(users_itemName).eq(user);
+    fe3 = Key(event_search).eq(event);
+    fe = Key(eventTime).between(time1, time2);
+    fe_complete = fe & fe2 & fe3
+    table = dynamodb_resource.Table(table_name)
+    response = table.scan(
+            ProjectionExpression=pe,
+            # ScanFilter=Key('eventTime').between(time1, time2)
+            FilterExpression= fe_complete,
+        )
+    events = response['Items']
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'], ProjectionExpression=pe,
+                                  FilterExpression=fe_complete, )
+        events.extend(response['Items'])
+
+    return events, len(events)
 
 
+"""Top users"""
+def top_users(time1, time2, event=None):
+    """aux method, return two dict - 
+    user and his events
+    user and his number of total events (counted)"""
+    def events_per_users(events):
+        print("-> Events per user")
+        # usera key = user
+        #value = list of events
+        users_events = dict()
+        users_events_count = dict()
+        for e in events:
+            user = e.get('userIdentity_userName',None)
+            if(user is None): continue #not all events have an user
+            event = e['eventName']
+            levent_user = users_events.get(user,[])
+            n_events_user = users_events_count.get(user,0)
+
+            levent_user.append(event)
+            n_events_user = n_events_user + 1
+
+            users_events[user] = levent_user
+            users_events_count[user] = n_events_user
+
+        return users_events,users_events_count
+
+    if not event:
+        actions = actions_between_time(time1, time2)
+        print("Actions between time done")
+
+    else:
+        fe_aux = Key('eventName').eq(event);
+        actions = actions_between_time(time1, time2, feAux= fe_aux)
+    print("events per user to do")
+    users_events, users_events_count = events_per_users(actions)
+    print("-"*10)
+    list_ordered_top_events = []
+
+
+    for key, value in users_events_count.items():
+        list_ordered_top_events.append([key,value])
+    list_ordered_top_events = sorted(list_ordered_top_events, key=lambda x: -x[1])
+
+    return actions,list_ordered_top_events
 
 def main():
     # table_name = 'EventoCloudTrail_230'
@@ -229,11 +318,14 @@ def main():
     # print("Time elapsed for all items %f " % elapsed_time)
 
     start_time = time.time()
-    user_list = used_services("gmolto",'2017-01-01T14:35:21Z','2017-10-01T14:35:21Z')
+    user_events,ordered_list = top_users('2017-06-01','2017-06-02', event = 'DescribeInstanceStatus')
     elapsed_time = time.time() - start_time
+    print("Top users")
+    print(user_events)
+    print(ordered_list)
     print("Time elapsed for  items %f " % elapsed_time)
 
-    print(user_list)
+
     # print(all)
     # print(len(all))
 
