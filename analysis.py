196649b3555194eb196e5fddba8a9932fb84d3c1
JoseRPrietoF <prietofontcuberta@gmail.com>
2017-11-12 19:18:36 +0100

    argparse chunk

--- analysis.py
+++ analysis.py
@@ -7,11 +7,13 @@ import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--path", help="Path that contains items to start the analysis", default='./examples')
+parser.add_argument("--porc_chunk", help="Split the data. 0.1 = splits of 10%. Default 0.1", default=0.1)
 
 def chunks(l, porc=0.1):
     """Yield successive n-sized chunks from l."""
-
+    print(int(len(l)))
     percent = max(1,int(len(l) * porc))
+    print(percent)
     for i in range(0, len(l)+1, percent):
         if i + percent+1 >= len(l):
             yield l[i : ]
@@ -87,17 +89,16 @@ def save_array(data, path):
             outfile.write('\n')
 
 """ Save events from a dir ans calculate times"""
-def analysis_events(path, table_name = 'EventoCloudTrail_230'):
+def analysis_events(path, porc_chunk = 0.1, table_name = 'EventoCloudTrail_230'):
     #Get all events from path
-    args = parser.parse_args()
-    path = args.path
+
     print("Path of files: %s with %d files" % (path, len(path)))
     events = get_structure(path)
     #split in equal parts (except last)
     #matrix for saves times
     save_times = []
     number_events = 0
-    for l in chunks(events, 0.1):
+    for l in chunks(events, porc_chunk):
         time_chunk = [None]*9
         #All times for chunck,
         #[NUMBER_EVENTS, T_Upload, user_events, user_events_service, user_list, top_users, act_between,times,
@@ -107,8 +108,8 @@ def analysis_events(path, table_name = 'EventoCloudTrail_230'):
         count = 0
         for e in l: #e = events file
             count = count + 1
-            print(" ------------------------------ Number %d" % count)
             event = Event(e)
+            print(" -------------------------File Number %d with %d events " % (count, event.count_events()))
             number_events = number_events + event.count_events()
             db = UseDynamoDB("prueba", verbose=False)
             db.guardar_evento(table_name, event)
@@ -134,8 +135,10 @@ def analysis_events(path, table_name = 'EventoCloudTrail_230'):
     save_array(save_times, 'times/times')
 
 def main():
-    path = 'examples'
-    analysis_events(path)
+    args = parser.parse_args()
+    path = args.path
+    porc_chunk = args.porc_chunk
+    analysis_events(path, porc_chunk)
     # a = [[1, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0], [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8, 9], [9, 8, 7, 6, 5, 4, 3, 2, 1]]
     # save_array(a,'times/times')
 
