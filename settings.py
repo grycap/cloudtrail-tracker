cab98825fdeea1a48adba71fbc7ea7643d83894e
Jose R. Prieto <prietofontcuberta@gmail.com>
2017-11-23 18:21:16 +0100

    Querys from V3

--- settings.py
+++ settings.py
@@ -3,3 +3,4 @@ table_name = "EventoCloudTrail_V3"
 
 """eventName 's that we DONT want to store - Filter"""
 filterEventNames = ["get", "describe", "list", "info", "decrypt"]
+index = 'userIdentity_userName-eventTime-index'
