17b6ee37bf85e35cf70b3ae0ad7969e1eb2a9798
Jose R. Prieto <prietofontcuberta@gmail.com>
2018-03-20 18:09:41 +0100

    M5. Added eventName in some queries

--- dynamodb/Querys.py
+++ dynamodb/Querys.py
@@ -182,6 +182,7 @@ def used_services(user, time1=None, time2=None,count=False):
     users_itemName = 'userIdentity_userName'
     eventTime = 'eventTime'
     eventsource = "eventSource"
+    eventName = "eventName"
 
     # filter expression
     feAux = Key(users_itemName).eq(user);
@@ -207,7 +208,7 @@ def used_services(user, time1=None, time2=None,count=False):
             )
             events = events + (response['Count'])
     else:
-        pe = "{0}, {1}, {2}".format(users_itemName, eventTime, eventsource)
+        pe = "{0}, {1}, {2}, {3}".format(users_itemName, eventTime, eventsource, eventName)
         response = table.query(
             IndexName=index,
             KeyConditionExpression=feAux,
@@ -238,6 +239,7 @@ def used_services_parameter(user, request_parameter, time1=None, time2=None, cou
     users_itemName = 'userIdentity_userName'
     eventTime = 'eventTime'
     eventsource = "eventSource"
+    eventName = "eventName"
 
     # filter expression
     feAux = Key(users_itemName).eq(user);
@@ -272,7 +274,7 @@ def used_services_parameter(user, request_parameter, time1=None, time2=None, cou
             events = events + (response['Count'])
 
     else:
-        pe = "{0}, {1}, {2}".format(users_itemName, eventTime, eventsource)
+        pe = "{0}, {1}, {2}, {3}".format(users_itemName, eventTime, eventsource, eventName)
         response = table.query(
             IndexName=index,
             KeyConditionExpression=feAux,
@@ -396,17 +398,17 @@ def main():
     # print(user_events)
     # print("Time elapsed for user_count_event items %f " % elapsed_time)
     #
-    start_time = time.time()
-    user_events = used_services('alucloud171','2014-06-01T12:00:51Z', '2017-06-01T19:00:51Z', count=False)
-    elapsed_time = time.time() - start_time
-    print(user_events)
-    print("Time elapsed for used_services items %f " % elapsed_time)
-
     # start_time = time.time()
-    # user_events = used_services_parameter('gmolto', request, '2014-06-01T12:00:51Z', '2018-06-01T19:00:51Z', count=False)
+    # user_events = used_services('alucloud171','2014-06-01T12:00:51Z', '2017-06-01T19:00:51Z', count=False)
     # elapsed_time = time.time() - start_time
     # print(user_events)
-    # print("Time elapsed for used_services_parameter items %f " % elapsed_time)
+    # print("Time elapsed for used_services items %f " % elapsed_time)
+
+    start_time = time.time()
+    user_events = used_services_parameter('gmolto', request, '2014-06-01T12:00:51Z', '2018-06-01T19:00:51Z', count=False)
+    elapsed_time = time.time() - start_time
+    print(user_events)
+    print("Time elapsed for used_services_parameter items %f " % elapsed_time)
 
 
     # start_time = time.time()
